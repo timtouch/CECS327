9dca7082c3f5d08291bbd27ee8cbaf06700720b0
Timothy Touch <chaosbounder@gmail.com>
2017-03-21 17:10:50 -0700

    Added class descriptions as well as console logs for debugging

--- src/Assignment3Client.java
+++ src/Assignment3Client.java
@@ -1,9 +1,14 @@
 /**
- * Created by ttouc on 2/27/2017.
+ * Author: Timothy Touch
+ *
+ *
+ * Description:
+ *  A client program that sends a series of numbers from 1-20 to the specified server.  After it has sent a message
+ *  it will wait for a response from the server, print out the response, and proceed to send the next sequential
+ *  number.
  */
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 public class Assignment3Client {
 
@@ -15,21 +20,24 @@ public class Assignment3Client {
         DatagramSocket socket = new DatagramSocket();
 
         byte[] buf = new byte[256];
-        InetAddress address = InetAddress.getByName(args[0]);
+        InetAddress address = InetAddress.getByName(args[0]); // Save address of server that we want to send a message to
         try {
             for (Integer i = 1; i <= 20; i++) { // Sends a sequence of numbers to server
                 String number = i.toString();
-                buf = number.getBytes();
+                buf = number.getBytes(); // Basically convert the int to a String to a byte to send in the Datagram Packet
                 //These DatagramPackets are a wrapper class
                 DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
-                socket.send(packet);
+                socket.send(packet);  // Send packet to server
 
-                packet = new DatagramPacket(buf, buf.length);
-                socket.receive(packet);
+                packet = new DatagramPacket(buf, buf.length); // Create a new packet
+                socket.receive(packet);  // Wait to receive a response from the server
 
                 String received = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("Number echoed: " + received);
+                Thread.sleep(1000);
             }
+        } catch (InterruptedException e){
+            e.printStackTrace();
         } finally {
             socket.close();
         }
--- src/Assignment3ServerThread.java
+++ src/Assignment3ServerThread.java
@@ -1,14 +1,16 @@
 /**
- * Created by ttouc on 2/27/2017.
+ * Author: Timothy Touch
+ *
+ * Description:
+ *  Server Thread meant to listen for requests from clients.  Once it receives a request, it echoes the message that the
+ *  client sent back to the client.
  */
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 public class Assignment3ServerThread extends Thread {
     protected DatagramSocket socket = null;
-    protected BufferedReader in = null;
-    protected boolean moreQuotes = true;
+    protected int listenSocket = 4445;
 
     public Assignment3ServerThread() throws IOException {
         this("ServerThread");
@@ -16,27 +18,28 @@ public class Assignment3ServerThread extends Thread {
 
     public Assignment3ServerThread(String name) throws IOException {
         super(name);
-        socket = new DatagramSocket(4445);
-
+        socket = new DatagramSocket(listenSocket);
     }
 
     public void run() {
         try {
+            System.out.println("The server is listening in port " + socket.getLocalPort());
             while (true) { // The server wil always listen
                 try {
                     byte[] buf = new byte[256];
 
-                    DatagramPacket packet = new DatagramPacket(buf, buf.length); //The message content: sent/received
-                    socket.receive(packet);  //Waiting for message
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length); // The message content: sent/received
+                    socket.receive(packet);  // Waiting for message
 
-                    String dString = new String(packet.getData(), 0, packet.getLength());
-                    System.out.println("Received " + dString + " from client address: " + packet.getAddress());
+                    String dString = new String(packet.getData(), 0, packet.getLength()); // Grab the message from the Datagram
+                    System.out.printf("Received \"%s\" from client address: %s%n", dString, packet.getAddress());
 
-                    buf = dString.getBytes();
-                    InetAddress address = packet.getAddress();
-                    int port = packet.getPort();
-                    packet = new DatagramPacket(buf, buf.length, address, port);  //Constructs the DatagramPacket for client
-                    socket.send(packet);  //Sending the packet back to client
+                    buf = dString.getBytes(); // Convert string to bytes
+                    InetAddress address = packet.getAddress(); // Get address of client we are going to send back to
+                    int port = packet.getPort(); // Get port of client
+                    packet = new DatagramPacket(buf, buf.length, address, port);  //Constructs the DatagramPacket address to the client
+                    System.out.printf("Sending \"%s\" to client address: %s%n", dString, packet.getAddress());
+                    socket.send(packet);  // Sending the packet back to client
 
                 } catch (IOException e) {
                     e.printStackTrace();
